bcbbb02cb14fb8cf1a8c155dfef4738aa17adba0
Mohit Nargotra <mohitn2804@gmail.com>
2023-03-22 18:45:00 -0400

    Fixed css for button

--- inc/sview.php
+++ inc/sview.php
@@ -15,9 +15,7 @@ class sview{
         add_action('save_post', array($this, 'save_sview_post'));
         add_shortcode('sview', array($this, 'sview_shortcode'));
         //add_action('admin_menu', array($this,'quiz_plugin_menu'));
-    }
-
-    
+    }    
 
     //registers custom post type
     function register_post_type(){
@@ -51,41 +49,10 @@ class sview{
     }
 
     //creates the metaboxes 
-    function register_meta_boxes(){
-        //add_meta_box('quiz_time_limit','Quiz Time Limit',array($this, 'quiz_time_limit_html'),'quiz');
-        //add_meta_box('question_password', 'Quiz Password', array($this, 'quiz_password_html'), 'quiz');
-        add_meta_box('quiz_meta', 'Quizzes for student view', array($this, 'quizzes_html'), 'sview');
-        //add_meta_box('quizzes_link_meta', 'Link for the quiz', array($this, 'quizzes_html'), 'sview');
+    function register_meta_boxes(){        
+        add_meta_box('quiz_meta', 'Quizzes for student view', array($this, 'quizzes_html'), 'sview');    
     }
 
-    // quiz time limit meta box
-/*     function quiz_time_limit_html($post){
-        wp_nonce_field('quiz_time_limit_field', 'quiz_nonce');
-        $time = get_post_meta( $post->ID, '_quiz_time_limit_meta_key', true );
-        if($time == ''){
-            $time = 60;
-        }
-        ?>
-        <div class="row">
-        <label for="quiz_time_limit_field"></label>
-        <input style='width:25%' type='number' name='quiz_time_limit_field' min="10" value="<?php echo esc_attr($time); ?>">
-        </div>
-        <?php
-    } */
-
-    // quiz password metabox
-
-    
-   /*  function quiz_password_html($post){
-        wp_nonce_field('quiz_password_field', 'quiz_nonce');
-        $password = get_post_meta( $post->ID, '_quiz_password_meta_key', true );
-        ?>
-        <div class="row">
-        <label for="quiz_password_field"></label>
-        <input style='width:25%' type='password' name='quiz_password_field' value="<?php echo esc_attr($password); ?>">
-        </div>
-        <?php
-    } */
 
     //quiz  question metabox
     function quizzes_html($post){
@@ -264,25 +231,20 @@ class sview{
             $q_date = array_values($q_date);
             $q_values = array_values($q_values);
         }
-
         ?>
         
 
 
-<table class="greyGridTable">
+<table class="minimalistBlack">
   <thead>
     <tr>
       <th>Name</th>
       <th>Author</th>
-      <th>Date Available </th>
+      <th>Date</th>
       <th></th>
     </tr>
-  </thead>  
-  
-
-
-        <?php
-         
+  </thead>    
+        <?php         
         for($i = 0; $i < $count; $i++){
             $key_name =$q_key[$i];
             $key_author =$q_author[$i];
@@ -294,19 +256,16 @@ class sview{
             <td><?php echo $key_name; ?></td>
             <td><?php echo $key_author; ?></td>
             <td><?php echo $key_date; ?></td>
-            <td><a href="<?php echo $key_link;?>"><button>Start Quiz</button></a></td>
+            <td><a href="<?php echo $key_link;?>" class="myButton">Start Quiz</a></td>
             </tr>
-            </tbody>
-            </table>                
+            </tbody>                           
             <?php
        }
         ?>            
-        
+        </table> 
         <hr class="wp-block-separator has-text-color has-css-opacity has-background is-style-dots">              
         <?php
-        return ob_get_clean();
-       
-           
+        return ob_get_clean();                 
 
 }
 }
