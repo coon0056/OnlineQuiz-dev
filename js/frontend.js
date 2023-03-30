6c9a20b290179eb50df95c772cefed07b19bb376
coon0056 <coon0056@algonquinlive.com>
2023-03-29 20:42:55 -0400

    modal css, modified quiz_cpt formatting and modal creation, frontend alert WIP

--- js/frontend.js
+++ js/frontend.js
@@ -12,14 +12,54 @@ jQuery(document).ready(function(){
         }
     });
 
-    //TODO
-    //jQuery('#user_question_submit').click(function(e){
-        // if question is unanswered
-        //if(jQuery("#question").find('input:invalid')){
-            //e.preventDefault();
-            //console.log(jQuery("#question").find('input:invalid').attr('id'));
-            //console.log(jQuery("#question").find('input:invalid').closest("div").attr('name'));
-            //alert(jQuery("input").find(":invalid").first());
-        //}
-    //});
+    jQuery('#user_question_submit').click(function(e){
+        //console.log(jQuery("#questions").find('input:invalid,select:invalid'));
+
+        jQuery("#question").each(function(index, element) {
+            console.log(jQuery(element));
+            e.preventDefault();
+            if(jQuery(element).find('input:invalid,select:invalid').length > 0){
+                e.preventDefault();
+    
+                jQuery('#modalMessage').text("Question "+ (jQuery("#questions").find('input:invalid,select:invalid').closest('li').data('num') + 1) + 
+                    " is unanswered. Please fill out all questions before submitting the quiz.")
+                jQuery('#modal').show();
+    
+                var modal = document.getElementById("modal");
+                modal.style.display = "block";
+                jQuery('#close').click(function(){
+                    modal.style.display = "none";
+                });
+    
+                window.onclick = function(e){
+                    if (e.target == modal) {
+                        modal.style.display = "none";
+                    }
+                }
+            } else {
+                console.log("fail");
+            }
+        });
+
+        // if(jQuery("#questions").find('input:invalid,select:invalid').length > 0){
+        //     e.preventDefault();
+
+        //     jQuery('#modalMessage').text("Question "+ (jQuery("#questions").find('input:invalid,select:invalid').closest('li').data('num') + 1) + 
+        //         " is unanswered. Please fill out all questions before submitting the quiz.")
+        //     jQuery('#modal').show();
+
+        //     var modal = document.getElementById("modal");
+        //     modal.style.display = "block";
+        //     jQuery('#close').click(function(){
+        //         modal.style.display = "none";
+        //     });
+
+        //     window.onclick = function(e){
+        //         if (e.target == modal) {
+        //             modal.style.display = "none";
+        //         }
+        //     }
+        // }
+    });
+
 });
